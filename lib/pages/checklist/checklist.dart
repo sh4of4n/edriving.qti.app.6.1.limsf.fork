e3bc574f6138eb674ba449d3842ee5df1a0aee36
joshua1996 <kjw1996@hotmail.com>
2022-10-17 17:56:01 +0800

    Make QTI/QTO tick only for demerit

--- lib/pages/checklist/checklist.dart
+++ lib/pages/checklist/checklist.dart
@@ -32,12 +32,8 @@ class _CheckListPageState extends State<CheckListPage> {
   List<JpjCheckListJson> checklistSkimArr = [];
   List<JpjCheckListJson> checklistLitarArr = [];
   List<JpjCheckListJson> checklistSystemArr = [];
-  bool skimCheck = false;
   bool litarCheck = false;
   bool sistemCheck = false;
-  bool skimCheckSelectAll = false;
-  bool litarCheckSelectAll = false;
-  bool sistemCheckSelectAll = false;
 
   final _formKey = GlobalKey<FormBuilderState>();
   final customDialog = CustomDialog();
@@ -219,13 +215,12 @@ class _CheckListPageState extends State<CheckListPage> {
       bool isUntickMandatory = false;
 
       for (var element in checklist[0].data) {
-        if (element.mandatory == 'true' &&
-            (element.isCheck == null || element.isCheck == false)) {
+        if (element.mandatory == 'true' && element.isCheck == true) {
           isUntickMandatory = true;
         }
         checklistSkimArr.add(JpjCheckListJson(
           checkCode: element.checkCode,
-          status: (element.isCheck != null && element.isCheck) ? '1' : '0',
+          status: (element.isCheck != null && element.isCheck) ? '0' : '1',
           remark: '',
         ));
       }
@@ -255,11 +250,6 @@ class _CheckListPageState extends State<CheckListPage> {
         }
 
         EasyLoading.dismiss();
-        if (mounted) {
-          setState(() {
-            skimCheck = true;
-          });
-        }
 
         if (isUntickMandatory) {
           await showDialog(
@@ -316,13 +306,12 @@ class _CheckListPageState extends State<CheckListPage> {
     bool isUntickMandatory = false;
 
     for (var element in checklist[1].data) {
-      if (element.mandatory == 'true' &&
-          (element.isCheck == null || element.isCheck == false)) {
+      if (element.mandatory == 'true' && element.isCheck == true) {
         isUntickMandatory = true;
       }
       checklistLitarArr.add(JpjCheckListJson(
         checkCode: element.checkCode,
-        status: (element.isCheck != null && element.isCheck) ? '1' : '0',
+        status: (element.isCheck != null && element.isCheck) ? '0' : '1',
         remark: '',
       ));
     }
@@ -403,13 +392,12 @@ class _CheckListPageState extends State<CheckListPage> {
     bool isUntickMandatory = false;
 
     for (var element in checklist[2].data) {
-      if (element.mandatory == 'true' &&
-          (element.isCheck == null || element.isCheck == false)) {
+      if (element.mandatory == 'true' && element.isCheck == true) {
         isUntickMandatory = true;
       }
       checklistSystemArr.add(JpjCheckListJson(
         checkCode: element.checkCode,
-        status: (element.isCheck != null && element.isCheck) ? '1' : '0',
+        status: (element.isCheck != null && element.isCheck) ? '0' : '1',
         remark: '',
       ));
     }
@@ -569,7 +557,7 @@ class _CheckListPageState extends State<CheckListPage> {
                                     child: Row(
                                       children: [
                                         Text(
-                                          "SKIM",
+                                          "Pemeriksaan Kenderaan Ujian",
                                           style: TextStyle(
                                             fontSize: 20,
                                             fontWeight: FontWeight.bold,
@@ -581,39 +569,17 @@ class _CheckListPageState extends State<CheckListPage> {
                                             var controller =
                                                 ExpandableController.of(context,
                                                     required: true)!;
-                                            return skimCheck
-                                                ? ElevatedButton.icon(
-                                                    onPressed: () {
-                                                      if (!controller
-                                                          .expanded) {
-                                                        controller.toggle();
-                                                      }
-                                                      updateJpjCheckListSkimA();
-                                                    },
-                                                    icon: Icon(
-                                                      Icons.check_circle,
-                                                    ),
-                                                    label: Text(AppLocalizations
-                                                            .of(context)!
-                                                        .translate('updated')),
-                                                    style: ElevatedButton
-                                                        .styleFrom(
-                                                      backgroundColor:
-                                                          Colors.green,
-                                                    ),
-                                                  )
-                                                : ElevatedButton(
-                                                    onPressed: () {
-                                                      if (!controller
-                                                          .expanded) {
-                                                        controller.toggle();
-                                                      }
-                                                      updateJpjCheckListSkimA();
-                                                    },
-                                                    child: Text(AppLocalizations
-                                                            .of(context)!
-                                                        .translate('update')),
-                                                  );
+                                            return ElevatedButton(
+                                              onPressed: () {
+                                                if (!controller.expanded) {
+                                                  controller.toggle();
+                                                }
+                                                updateJpjCheckListSkimA();
+                                              },
+                                              child: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .translate('update')),
+                                            );
                                           },
                                         ),
                                       ],
@@ -713,29 +679,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                           ),
                                         ),
                                       ),
-                                      CheckboxListTile(
-                                        title: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            Text(AppLocalizations.of(context)!
-                                                .translate('select_all')),
-                                          ],
-                                        ),
-                                        value: skimCheckSelectAll,
-                                        onChanged: (bool? value) {
-                                          setState(() {
-                                            skimCheckSelectAll = value ?? false;
-                                            for (var element
-                                                in checklist[0].data) {
-                                              element.isCheck = value;
-                                            }
-                                          });
-                                        },
-                                      ),
-                                      Divider(
-                                        height: 1,
-                                      ),
                                       ListView.separated(
                                         shrinkWrap: true,
                                         physics: NeverScrollableScrollPhysics(),
@@ -771,18 +714,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                                 checklist[0]
                                                     .data[index]
                                                     .isCheck = value;
-                                                int checkCount = checklist[0]
-                                                    .data
-                                                    .where((i) =>
-                                                        i.isCheck != null &&
-                                                        i.isCheck)
-                                                    .toList()
-                                                    .length;
-                                                skimCheckSelectAll =
-                                                    checkCount ==
-                                                        checklist[0]
-                                                            .data
-                                                            .length;
                                               });
                                             },
                                           );
@@ -895,27 +826,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: <Widget>[
-                                      CheckboxListTile(
-                                        title: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            Text(AppLocalizations.of(context)!
-                                                .translate('select_all')),
-                                          ],
-                                        ),
-                                        value: litarCheckSelectAll,
-                                        onChanged: (bool? value) {
-                                          setState(() {
-                                            litarCheckSelectAll =
-                                                value ?? false;
-                                            for (var element
-                                                in checklist[1].data) {
-                                              element.isCheck = value;
-                                            }
-                                          });
-                                        },
-                                      ),
                                       ListView.separated(
                                         shrinkWrap: true,
                                         physics: NeverScrollableScrollPhysics(),
@@ -951,19 +861,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                                 checklist[1]
                                                     .data[index]
                                                     .isCheck = value;
-
-                                                int checkCount = checklist[1]
-                                                    .data
-                                                    .where((i) =>
-                                                        i.isCheck != null &&
-                                                        i.isCheck)
-                                                    .toList()
-                                                    .length;
-                                                litarCheckSelectAll =
-                                                    checkCount ==
-                                                        checklist[1]
-                                                            .data
-                                                            .length;
                                               });
                                             },
                                           );
@@ -1065,27 +962,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: <Widget>[
-                                      CheckboxListTile(
-                                        title: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            Text(AppLocalizations.of(context)!
-                                                .translate('select_all')),
-                                          ],
-                                        ),
-                                        value: sistemCheckSelectAll,
-                                        onChanged: (bool? value) {
-                                          setState(() {
-                                            sistemCheckSelectAll =
-                                                value ?? false;
-                                            for (var element
-                                                in checklist[2].data) {
-                                              element.isCheck = value;
-                                            }
-                                          });
-                                        },
-                                      ),
                                       ListView.separated(
                                         shrinkWrap: true,
                                         physics: NeverScrollableScrollPhysics(),
@@ -1121,19 +997,6 @@ class _CheckListPageState extends State<CheckListPage> {
                                                 checklist[2]
                                                     .data[index]
                                                     .isCheck = value;
-
-                                                int checkCount = checklist[2]
-                                                    .data
-                                                    .where((i) =>
-                                                        i.isCheck != null &&
-                                                        i.isCheck)
-                                                    .toList()
-                                                    .length;
-                                                sistemCheckSelectAll =
-                                                    checkCount ==
-                                                        checklist[2]
-                                                            .data
-                                                            .length;
                                               });
                                             },
                                           );
--- lib/pages/checklist/checklist_result.dart
+++ lib/pages/checklist/checklist_result.dart
@@ -110,7 +110,7 @@ class _ChecklistResultPageState extends State<ChecklistResultPage> {
                                     header: Padding(
                                       padding: EdgeInsets.all(10),
                                       child: Text(
-                                        "SKIM",
+                                        "Pemeriksaan Kenderaan Ujian",
                                         style: TextStyle(
                                           fontSize: 20,
                                           fontWeight: FontWeight.bold,
@@ -293,8 +293,8 @@ class _ChecklistResultPageState extends State<ChecklistResultPage> {
                                                             value:
                                                                 item.status ==
                                                                         'true'
-                                                                    ? true
-                                                                    : false,
+                                                                    ? false
+                                                                    : true,
                                                             onChanged: (bool?
                                                                 value) {},
                                                           ),
@@ -394,8 +394,8 @@ class _ChecklistResultPageState extends State<ChecklistResultPage> {
                                             child: CheckboxListTile(
                                               title: Text(item.checkDesc),
                                               value: item.status == 'true'
-                                                  ? true
-                                                  : false,
+                                                  ? false
+                                                  : true,
                                               onChanged: (bool? value) {},
                                             ),
                                           ),
@@ -463,8 +463,8 @@ class _ChecklistResultPageState extends State<ChecklistResultPage> {
                                             child: CheckboxListTile(
                                               title: Text(item.checkDesc),
                                               value: item.status == 'true'
-                                                  ? true
-                                                  : false,
+                                                  ? false
+                                                  : true,
                                               onChanged: (bool? value) {},
                                             ),
                                           ),
